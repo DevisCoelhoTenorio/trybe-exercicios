32cf9c0c9e55f5d5a732c6b9be2ae6738cf3c161
DevisCoelhoTenorio <devis.ufal@gmail.com>
2022-03-07 15:53:22 -0300

    Para 02 fixar 4.1

---
+++ trybe-exercicios/1-Fundamentos/4-Introdução à JavaScript e Lógica de Programação/scripts2.js
@@ -0,0 +1,22 @@
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+    fistName: 'Ana',
+    lastName: 'Santos',
+};
+const patientEmail ='ana@email.com';
+
+console.log(typeof patientEmail);
+console.log(typeof patientId);
+console.log(typeof isEnrolled);
+console.log(typeof patientInfo);
+console.log(typeof patientAge);
+
+const base = 5;
+const height = 8;
+const area = base * height;
+
+console.log (area);
+let perimeter = (base ** 2) + (height ** 2);
+perimeter = Math.sqrt(perimeter);
+console.log (perimeter); 
