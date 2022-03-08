33bd367ffbfffd856704212710e7b67765c16e57
DevisCoelhoTenorio <devis.ufal@gmail.com>
2022-03-08 18:23:42 -0300

    Aprofunde seus conhecimentos requisto 2

--- trybe-exercicios/1-Fundamentos/4-Introdução à JavaScript e Lógica de Programação/dia-2-javascript-array-e-loop-for/AprofundeConhecimento.js
+++ trybe-exercicios/1-Fundamentos/4-Introdução à JavaScript e Lógica de Programação/dia-2-javascript-array-e-loop-for/AprofundeConhecimento.js
@@ -1,6 +1,15 @@
 let numbers=[5,9,3,19,70,8,100,2,35,27];
 
+var total=0;
+
 for (var cont = 0; cont < numbers.length; cont+= 1){
+
+    total= total + numbers[cont];
+
+    if(cont === numbers.length-1){
+
+        console.log(total);
+    }
     
-    console.log(numbers[cont]);
+    
 }
