e8bf3b58fc7fc14d07a29713aa29021b8b2ce071
DevisCoelhoTenorio <devis.ufal@gmail.com>
2022-03-08 19:22:21 -0300

    Aprofunde seus conhecimentos requisto 5

--- trybe-exercicios/1-Fundamentos/4-Introdução à JavaScript e Lógica de Programação/dia-2-javascript-array-e-loop-for/AprofundeConhecimento.js
+++ trybe-exercicios/1-Fundamentos/4-Introdução à JavaScript e Lógica de Programação/dia-2-javascript-array-e-loop-for/AprofundeConhecimento.js
@@ -1,24 +1,39 @@
 let numbers=[5,9,3,19,70,8,100,2,35,27];
 
-var total=0;
+let max=[0];
 
-for (var cont = 0; cont < numbers.length; cont+= 1){
+//var total=0;
 
-    total= total + numbers[cont];
-
-    if(cont === numbers.length-1){
-
-        if(total/numbers.length > 20){
-
-            console.log("Valor maior que 20");
-        }
-        else{
-
-            console.log("Valor menor que 20");
-        }
-
-        console.log(total/numbers.length);
+for(var cont =0; cont < numbers.length; cont +=1){
+    if(numbers[cont]>max){
+        max =numbers[cont];
     }
-    
-    
 }
+
+
+console.log(max);
+    
+
+
+
+
+
+
+
+    // total= total + numbers[cont];
+
+    // if(cont === numbers.length-1){
+
+    //     if(total/numbers.length > 20){
+
+    //         console.log("Valor maior que 20");
+    //     }
+    //     else{
+
+    //         console.log("Valor menor que 20");
+    //     }
+
+    //     console.log(total/numbers.length);
+    // }
+    
+
