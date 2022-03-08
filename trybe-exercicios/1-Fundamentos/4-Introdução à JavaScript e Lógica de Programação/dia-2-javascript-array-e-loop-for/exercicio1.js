d839e77a47ccdf623b3ce77f65d73d7772ab33e6
DevisCoelhoTenorio <devis.ufal@gmail.com>
2022-03-08 15:50:21 -0300

    exercicio 3

--- trybe-exercicios/1-Fundamentos/4-Introdução à JavaScript e Lógica de Programação/dia-2-javascript-array-e-loop-for/exercicio1.js
+++ trybe-exercicios/1-Fundamentos/4-Introdução à JavaScript e Lógica de Programação/dia-2-javascript-array-e-loop-for/exercicio1.js
@@ -2,6 +2,9 @@ let menu =['Home','Serviço', 'Portifólio', 'Links'];
 
 let menuservices= menu[1];
 
+menu.push('Contato');
+
 console.log(menuservices);
 
 console.log(menu.indexOf('Portifólio'));
+console.log(menu);
