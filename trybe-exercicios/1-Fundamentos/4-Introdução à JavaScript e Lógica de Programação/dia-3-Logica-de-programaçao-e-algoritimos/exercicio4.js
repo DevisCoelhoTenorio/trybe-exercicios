d0061594a491b9afa7844c640300510b82c19445
DevisCoelhoTenorio <devis.ufal@gmail.com>
2022-03-09 20:45:09 -0300

    Exercicio 4

---
+++ trybe-exercicios/1-Fundamentos/4-Introdução à JavaScript e Lógica de Programação/dia-3-Logica-de-programaçao-e-algoritimos/exercicio4.js
@@ -0,0 +1,22 @@
+let numerosPrimos=[];
+let cont=0;
+let num=0;
+
+    for(let index= 2 ;index <=50; index +=1){
+
+        cont=0;
+
+       for (let index2=1; index2 <= index; index2 +=1){
+
+           if(index%index2 ===0 ){
+               cont += 1;
+
+           }
+           num=index;
+           }
+           if(cont === 2){
+            numerosPrimos.push(num);
+        }
+        }
+        
+console.log(numerosPrimos);
