ac679c0052d67640d9912dc110b2f350005d8076
DevisCoelhoTenorio <devis.ufal@gmail.com>
2022-03-09 17:33:07 -0300

    ADD

--- trybe-exercicios/1-Fundamentos/4-Introdução à JavaScript e Lógica de Programação/dia-2-javascript-array-e-loop-for/Bonus.js
+++ trybe-exercicios/1-Fundamentos/4-Introdução à JavaScript e Lógica de Programação/dia-2-javascript-array-e-loop-for/Bonus.js
@@ -1,12 +1,15 @@
 let numbers= [5,9,3,19,70,8,100,2,35,27];
 
+let snumbers=[];
+
 for (let index =1; index<numbers.length; index +=1){
     for (let sindex = 0; sindex <index; sindex +=1){
-        if(numbers[index]>numbers[sindex]){
-            let position = numbers[index];
-            numbers[index]=numbers[sindex];
-            numbers[sindex]=position;
+
+        
+
+            snumbers.push(numbers[sindex]*numbers[index]);
+            
         }
     }
-}
-console.log(numbers);
+
+console.log(snumbers);
