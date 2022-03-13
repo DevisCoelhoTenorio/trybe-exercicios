1e51bd25eb7c245ae31dde666fb7d3c19df41518
DevisCoelhoTenorio <devis.ufal@gmail.com>
2022-03-13 19:42:48 -0300

    Bonus 2

--- trybe-exercicios/1-Fundamentos/4-Introdução à JavaScript e Lógica de Programação/dia-3-Logica-de-programaçao-e-algoritimos/bonus1.js
+++ trybe-exercicios/1-Fundamentos/4-Introdução à JavaScript e Lógica de Programação/dia-3-Logica-de-programaçao-e-algoritimos/bonus1.js
@@ -1,15 +1,24 @@
-let num = 5;
-let quadrado =[];
+// let num = 5;
+// let quadrado =[];
 
-for (let index2 =0; index2 < num; index2 += 1){
+// for (let index2 =0; index2 < num; index2 += 1){
 
-    for (let index = 0; index < num; index += 1){
-    if(quadrado.length < num){
+//     for (let index = 0; index < num; index += 1){
+//     if(quadrado.length < num){
 
     
-    quadrado.push('*');
-    }
-    }
+//     quadrado.push('*');
+//     }
+//     }
 
-console.log(quadrado.join(''));
-}
+// console.log(quadrado.join(''));
+// }
+
+let tamanho = 5;
+let simbolo = '*';
+let linha ='';
+
+for (let index =0; index <= tamanho ; index +=1){
+    console.log(linha);
+    linha=linha + simbolo;
+};
