2acfefcd1c1136f6ef55820a3575b1228222a626
DevisCoelhoTenorio <devis.ufal@gmail.com>
2022-03-13 18:30:44 -0300

    Bonus requisito 2

--- trybe-exercicios/1-Fundamentos/4-Introdução à JavaScript e Lógica de Programação/dia-2-javascript-array-e-loop-for/Bonus.js
+++ trybe-exercicios/1-Fundamentos/4-Introdução à JavaScript e Lógica de Programação/dia-2-javascript-array-e-loop-for/Bonus.js
@@ -1,3 +1,5 @@
+// Bônus
+//01
 let numbers= [5,9,3,19,70,8,100,2,35,27];
 
 console.log(numbers.sort(function(a,b){
@@ -7,6 +9,16 @@ console.log(numbers.sort(function(a,b){
 
         return 0;
     }));
-
 // sort necessita de uma função para ordenar numeros.
 // portanto deve ser usasdo .sort(function(a,b){ })
+
+//02
+console.log(numbers.sort(function(a,b){
+        if(a > b) return -1; //inverter o retorno entre os if muda o ordenamento de crescente para decrescente
+
+        if(a <b) return 1;
+
+        return 0;
+    }));
+
+
