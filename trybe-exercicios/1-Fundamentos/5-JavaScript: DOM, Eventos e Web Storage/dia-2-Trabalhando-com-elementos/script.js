880eba89abcd02a4970c775f86e6c0181b7da4c4
DevisCoelhoTenorio <devis.ufal@gmail.com>
2022-03-15 15:54:14 -0300

    para fixar parte II

--- trybe-exercicios/1-Fundamentos/5-JavaScript: DOM, Eventos e Web Storage/dia-2-Trabalhando-com-elementos/script.js
+++ trybe-exercicios/1-Fundamentos/5-JavaScript: DOM, Eventos e Web Storage/dia-2-Trabalhando-com-elementos/script.js
@@ -1,1 +1,26 @@
-console.log(document.getElementById("primeiroFilhoDoFilho").parentNode);
+// 1
+const elementoOndeVoceEsta = document.getElementById("elementoOndeVoceEsta");
+//2
+const pai = elementoOndeVoceEsta.parentElement;
+
+pai.style.color = "red";
+//3
+const pirmeiroFilhofoFilho = elementoOndeVoceEsta.firstElementChild;
+
+pirmeiroFilhofoFilho.innerText = "peimeiroFilhoDoFilho";
+//4
+
+const primeiroFilho=pai.firstElementChild;
+//5
+const primeiroFilho2=elementoOndeVoceEsta.previousElementSibling;
+//6
+const textElement = elementoOndeVoceEsta.nextSibling;
+//7 
+const terceiroFilho= elementoOndeVoceEsta.nextElementSibling;
+//8
+const terceiroFilho2 = pai.lastElementChild.previousElementSibling;
+
+
+
+
+
