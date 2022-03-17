1f72b5f247106b0e282c42bec80fac1002e4bd25
DevisCoelhoTenorio <devis.ufal@gmail.com>
2022-03-16 21:16:00 -0300

    requisito 2 ,3 e 4

--- trybe-exercicios/1-Fundamentos/5-JavaScript: DOM, Eventos e Web Storage/dia-3-JavaScript-Eventos/main.js
+++ trybe-exercicios/1-Fundamentos/5-JavaScript: DOM, Eventos e Web Storage/dia-3-JavaScript-Eventos/main.js
@@ -25,12 +25,22 @@ thirdLi.addEventListener('click',techElemento);
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
 
+input.addEventListener('input',function(event){
+    const techElement = document.querySelector('.tech');
+    techElement.innerText=event.target.value;
+});
+
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portfólio?
 
+myWebpage.addEventListener('dblclick',function(){
+    window.open('https://github.com/DevisCoelhoTenorio?tab=repositories/');
+});
+
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
+
 
 // Segue abaixo um exemplo do uso de event.target:
 
