0e84e44f32370f26be5cc3fbbcc1fc43c86c6b6c
DevisCoelhoTenorio <devis.ufal@gmail.com>
2022-04-04 16:59:09 -0300

    Objetos Part I finalizado

--- trybe-exercicios/1-Fundamentos/7-Introdução à JavaScript ES6 e Testes Unitários/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/conteudo/Adicionando novas chaves.js
+++ trybe-exercicios/1-Fundamentos/7-Introdução à JavaScript ES6 e Testes Unitários/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/conteudo/Adicionando novas chaves.js
@@ -1,5 +1,10 @@
-const customer = {
-    firstName: 'Roberto',
-    age: 22,
-    job: 'Teacher',
-  };
+const criar = (objeto, nome1, valor1) => {
+  const objeto1 = {};
+  objeto1.nome = nome1;
+  objeto1.valor = valor1;
+  objeto = objeto1;
+  
+  console.log(objeto);
+};
+
+criar ('cadastro', 'devis', 30);
