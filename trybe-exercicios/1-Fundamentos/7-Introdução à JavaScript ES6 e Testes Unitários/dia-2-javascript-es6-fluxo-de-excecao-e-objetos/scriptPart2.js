e9f48a3c276d6d1e89f6f1cb529ba25907df9bf3
DevisCoelhoTenorio <devis.ufal@gmail.com>
2022-04-04 21:55:47 -0300

    Exercício Part II finalizado

--- trybe-exercicios/1-Fundamentos/7-Introdução à JavaScript ES6 e Testes Unitários/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/scriptPart2.js
+++ trybe-exercicios/1-Fundamentos/7-Introdução à JavaScript ES6 e Testes Unitários/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/scriptPart2.js
@@ -35,15 +35,25 @@ const order = {
   };
   
   const customerInfo = (order) => {
-    // Adicione abaixo as informações necessárias.
+    const deliveryPerson = order.order.delivery.deliveryPerson;
+    const nome = order.name;
+    const phone = order.phoneNumber;
+    const address = `${order.address.street}, Nº: ${order.address.number},AP: ${order.address.apartment}`;
+    console.log(`Olá ${deliveryPerson}, entrega para: ${nome}, Telefone: ${phone}, ${address}`);
   
   }
   
   customerInfo(order);
   
   const orderModifier = (order) => {
-    // Adicione abaixo as informações necessárias.
+  order.name = 'Luiz Silva';
+  const nome = order.name;
+  const pizza = Object.keys(order.order.pizza);
+  const drink = Object.keys(order.order.drinks);
+  let valor = order.order.pizza.marguerita.price + order.order.pizza.pepperoni.price + order.order.drinks.coke.price;
+  valor = 50;
   
+  console.log(`Olá ${nome}, o total do seu pedido de ${pizza[0]}, ${pizza[1]} e ${drink} é R$ ${valor}`);
   }
   
   orderModifier(order);
