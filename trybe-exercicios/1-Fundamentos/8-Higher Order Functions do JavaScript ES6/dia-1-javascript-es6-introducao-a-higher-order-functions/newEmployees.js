fc827ce71950b662177905c4a4659d1b13b4f4c4
DevisCoelhoTenorio <devis.ufal@gmail.com>
2022-04-07 16:24:53 -0300

    Prática newEmployees

---
+++ trybe-exercicios/1-Fundamentos/8-Higher Order Functions do JavaScript ES6/dia-1-javascript-es6-introducao-a-higher-order-functions/newEmployees.js
@@ -0,0 +1,15 @@
+const cadastrar = (nomeCompleto) => {
+  const email = nomeCompleto.toLowerCase().split(' ').join('_');
+  return {nomeCompleto, email: `${email}@trybe.com`};
+};
+
+const newEmployees = (func) => {
+  const employees = {
+    id1: func('Pedro Guerra'),
+    id2: func('Luiza Drumond'),
+    id3:func('Carla Paiva'),
+  }
+  return employees;
+};
+
+console.log(newEmployees(cadastrar));
