6cbf3d29de6297d1bb369d861e4ee38889d607fc
DevisCoelhoTenorio <devis.ufal@gmail.com>
2022-04-08 19:30:51 -0300

    para fixar 1 e 2 sort

---
+++ trybe-exercicios/1-Fundamentos/8-Higher Order Functions do JavaScript ES6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/ordenarIdade.js
@@ -0,0 +1,12 @@
+const people = [
+    { name: 'Mateus', age: 18 },
+    { name: 'José', age: 16 },
+    { name: 'Ana', age: 23 },
+    { name: 'Cláudia', age: 20 },
+    { name: 'Bruna', age: 19 },
+  ];
+  
+  // Adicione se código aqui
+  people.sort((nome1, nome2) => nome2.age - nome1.age);
+
+  console.log(people);
