c77efc3ede5d80991f853d81252678fc0ec8ea41
DevisCoelhoTenorio <devis.ufal@gmail.com>
2022-04-11 18:24:43 -0300

    iniciando exercicios  8.3

---
+++ trybe-exercicios/1-Fundamentos/8-Higher Order Functions do JavaScript ES6/dia-3-javascript-es6-higher-order-functions-map-e-filter/diferencaEntreForEMap.js
@@ -0,0 +1,17 @@
+const persons = [
+    { firstName: 'Maria', lastName: 'Ferreira' },
+    { firstName: 'João', lastName: 'Silva' },
+    { firstName: 'Antonio', lastName: 'Cabral' },
+  ];
+  
+  let  fullNames = [];
+  
+  for (let index = 0; index < persons.length; index += 1) {
+    fullNames.push(`${persons[index].firstName} ${persons[index].lastName}`);
+  }
+  
+  console.log(fullNames);
+
+  fullNames = persons.map((person) => `${person.firstName} ${person.lastName}`);
+
+console.log(fullNames);
