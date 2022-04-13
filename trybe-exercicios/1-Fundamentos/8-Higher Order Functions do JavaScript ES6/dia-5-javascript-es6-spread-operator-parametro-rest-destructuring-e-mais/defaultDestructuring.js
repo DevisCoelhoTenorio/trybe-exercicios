8c395337b22fdcf6650fbc027480a27afc3457b1
DevisCoelhoTenorio <devis.ufal@gmail.com>
2022-04-13 20:18:48 -0300

    praticando 1 e 2

--- trybe-exercicios/1-Fundamentos/8-Higher Order Functions do JavaScript ES6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/defaultDestructuring.js
+++ trybe-exercicios/1-Fundamentos/8-Higher Order Functions do JavaScript ES6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/defaultDestructuring.js
@@ -16,3 +16,10 @@ const [x, y, z = 0] = position2d;
 console.log(x); // 1
 console.log(y); // 2
 console.log(z); // 0
+// const numeros = [1,2,3,4,5,6];
+
+// const sum1 = (...param) => param.reduce((acc, valor) => acc + valor, 0);
+// console.log(sum1(numeros));
+
+// const sum = (param) => param.reduce((acc, valor) => acc + valor, 0);
+// console.log(sum(numeros));
---
+++ trybe-exercicios/1-Fundamentos/8-Higher Order Functions do JavaScript ES6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercise2.js
@@ -0,0 +1,3 @@
+const sum = (...valores) => valores.reduce((acc, valor) => acc + valor, 0);
+
+console.log(sum(1,2,3,4,5,6,7,8,9,10,10,30));
