00615bd5b82ea0b83474903102143b7b7dd9c642
DevisCoelhoTenorio <devis.ufal@gmail.com>
2022-04-13 19:33:45 -0300

    finalizando default Destru

---
+++ trybe-exercicios/1-Fundamentos/8-Higher Order Functions do JavaScript ES6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/defaultDestructuring.js
@@ -0,0 +1,18 @@
+const person = {
+    name: 'João',
+    lastName: 'Jr',
+    age: 34,
+  };
+  
+  const { nationality = 'Coreano' } = person;
+
+//   console.log(nationality || 'sem nacionalidade');
+
+console.log(nationality);
+
+const position2d = [1.0, 2.0];
+const [x, y, z = 0] = position2d;
+
+console.log(x); // 1
+console.log(y); // 2
+console.log(z); // 0
