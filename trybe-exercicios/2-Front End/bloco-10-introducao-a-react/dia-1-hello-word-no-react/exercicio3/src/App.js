9633b57f29c57b89bb7ba0204ae97b235eb83d11
DevisCoelhoTenorio <devis.ufal@gmail.com>
2022-05-06 19:32:35 -0300

    fixando exercicios 10.2 parte final2

--- trybe-exercicios/2-Front End/bloco-10-introducao-a-react/dia-1-hello-word-no-react/exercicio3/src/App.js
+++ trybe-exercicios/2-Front End/bloco-10-introducao-a-react/dia-1-hello-word-no-react/exercicio3/src/App.js
@@ -4,8 +4,9 @@ import './App.css';
 import Order from './Order';
 
 class App extends React.Component {
-  render() {
-    const headphone = {
+  render(){ 
+    const lista = [
+     {
       id: 102,
       user: "cena@gmail.com",
       product: "Razer Headphone",
@@ -13,9 +14,9 @@ class App extends React.Component {
         value: 99.99,
         currency: "dollars"
       }
-    };
+    },
 
-    const energyDrink = {
+    {
       id: 77,
       user: "cena@gmail.com",
       product: "Monster 500mL",
@@ -23,16 +24,18 @@ class App extends React.Component {
         value: 9.99,
         currency: "dollars"
       }
-    };
-
+    }
+  ];
+  
     return (
       <div className="App">
         <h1> Orders recently created </h1>
-         <Order order={headphone}/>
-         <Order order={energyDrink}/>
+       {lista.map((item, index) => <Order valores={item} key={index}/>)}
+       
       </div>
     );
   }
 }
+
 
 export default App;
--- trybe-exercicios/2-Front End/bloco-10-introducao-a-react/dia-1-hello-word-no-react/exercicio3/src/Order.jsx
+++ trybe-exercicios/2-Front End/bloco-10-introducao-a-react/dia-1-hello-word-no-react/exercicio3/src/Order.jsx
@@ -1,9 +1,11 @@
 // arquivo Order.js
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Order extends React.Component {
   render() {
-    const { user, product, price } = this.props.order;
+    const { user, product, price } = this.props.valores;
+   
 
     return (
       <div className="order">
@@ -11,6 +13,12 @@ class Order extends React.Component {
       </div>
     );
   }
+}
+Order.protoType = {
+    user: PropTypes.string,
+    product: PropTypes.string,
+    value: PropTypes.number,
+    currency: PropTypes.number,
 }
 
 export default Order;
