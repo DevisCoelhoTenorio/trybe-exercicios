c4ce28c8b2b5ab41eec8aacf4bcd5410ea71d586
DevisCoelhoTenorio <devis.ufal@gmail.com>
2022-05-09 13:32:58 -0300

    atualizando master

--- trybe-exercicios/2-Front End/bloco-10-introducao-a-react/dia-2-componentes-react/exercicio10-2/src/componente/Card.js
+++ trybe-exercicios/2-Front End/bloco-10-introducao-a-react/dia-2-componentes-react/exercicio10-2/src/componente/Card.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 class Card extends React.Component {
     render() {
-        const {id, name, type, averageWeight, image} = this.props.pokemon;
+        const {id, name, type, averageWeight: {value, measurementUnit}, image} = this.props.pokemon;
   return (
       <div className="card">
            <div>
                 <h1>{name}</h1>
                 <h2>{type}</h2>
-                <h3>{averageWeight.value}{averageWeight.measurementUnit}</h3>
+                <h3>{value}{measurementUnit}</h3>
             </div>
             <img src={image} alt={id}></img>
         </div>
