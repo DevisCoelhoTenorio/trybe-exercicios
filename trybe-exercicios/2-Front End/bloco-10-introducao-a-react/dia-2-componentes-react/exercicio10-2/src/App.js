cd0c84ddb999074374ab65067ca17c6177d4fe6c
DevisCoelhoTenorio <devis.ufal@gmail.com>
2022-05-10 14:34:10 -0300

    fechando dia 2 iniciando dia 1

--- trybe-exercicios/2-Front End/bloco-10-introducao-a-react/dia-2-componentes-react/exercicio10-2/src/App.js
+++ trybe-exercicios/2-Front End/bloco-10-introducao-a-react/dia-2-componentes-react/exercicio10-2/src/App.js
@@ -4,9 +4,13 @@ import Pokedex from './componente/Pokedex';
 
 function App() {
   return (
+    <div>
+    <header className="header">Pokédex</header>
     <main>
       <Pokedex />
     </main>
+    <footer>Dev.CT</footer>
+    </div>
   );
 }
 
