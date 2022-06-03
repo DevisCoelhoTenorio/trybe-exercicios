c4588f175b9ac0ba2ae739d96163cf77873691be
DevisCoelhoTenorio <devis.ufal@gmail.com>
2022-06-03 15:39:32 -0300

    dechando dia 1

--- trybe-exercicios/2-Front End/bloco-14-testes-automatizados-com-react-testing-library/dia-1-rtl-primeiros-passos/exercise-todo-list-master/src/test/App.test.js
+++ trybe-exercicios/2-Front End/bloco-14-testes-automatizados-com-react-testing-library/dia-1-rtl-primeiros-passos/exercise-todo-list-master/src/test/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 
 describe('Testando a App.js', () => {
-  test('Verificando se o label e o input existem no documento', () => {
+  it('Verificando se o label e o input existem no documento', () => {
     render(<App />);
     const labelTask = screen.getByText('Tarefa:');
     const inputTask = screen.getByLabelText('Tarefa:');
@@ -12,9 +13,25 @@ describe('Testando a App.js', () => {
     expect(inputTask).toBeInTheDocument();
   });
 
-  test('Verificando o tipo do input', () => {
+  it('Verificando o tipo do input', () => {
     render(<App />);
     const inputTask = screen.getByLabelText('Tarefa:');
     expect(inputTask.type).toBe('text');
+  });
+  it('Verificando o botão adicionar', () => {
+    render(<App />);
+    const buttonAdd = screen.getByRole('button');
+    expect(buttonAdd).toBeInTheDocument();
+    expect(buttonAdd).toHaveTextContent('Adicionar');
+  });
+  it('Verificando a funcionalidade do botão', () => {
+    render(<App />);
+    const buttonAdd = screen.getByRole('button');
+    const inputTask = screen.getByLabelText('Tarefa:');
+    const userList = screen.getByRole('list');
+    userEvent.type(inputTask, 'Tarefa teste');
+    userEvent.click(buttonAdd);
+    expect(inputTask).toHaveValue('');
+    expect(userList).toHaveTextContent(/Tarefa teste/i);
   });
 });
